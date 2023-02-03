846fdb611a5136a15cdba58e8175e837b398e7df
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-03 20:56:46 +0200

    create FormContact folders with files

---
+++ src/components/FormContact/FormContact.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Label, Input, Button } from './FormContact.styled';
+
+const FormContact = ({ onSubmit }) => {
+  const initialState = {
+    name: '',
+    number: '',
+  };
+  const [state, setState] = useState({ ...initialState });
+  const { name, number } = state;
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setState(prevState => {
+      return { ...prevState, [name]: value };
+    });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number });
+    setState({ ...initialState });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChange}
+        />
+      </Label>
+      <Label>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChange}
+        />
+      </Label>
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
+
+FormContact.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+export default FormContact;
